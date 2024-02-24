b376355389cecb12245d92cdc5ac4269f159980b
Deevesh Beegun <deeveshbeegun@Deeveshs-MacBook-Pro.local>
2024-02-24 15:22:00 +0400

    Add vader model

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
+from nltk.sentiment import SentimentIntensityAnalyzer
+from tqdm.notebook import tqdm
 
 app = Flask(__name__)
 
 @app.route('/analyse', methods=['POST'])
 def vader_analyse():
-	print('Hello Vader')
+	sentimentAnalyzer = SentimentIntensityAnalyzer()
+	text = request.form['text']
+	senti_dict = sentimentAnalyzer.polarity_scores(text)
 
+	return str(senti_dict["compound"])
 
 if __name__=='__main__':
 	app.run()
